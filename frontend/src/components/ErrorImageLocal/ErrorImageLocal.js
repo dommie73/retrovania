eb1e6d18af695384e7c5cbae37db632c203b3651
Reactor94 <v.pylypets94@gmail.com>
2020-06-27 21:25:59 +0200

    add data id

--- frontend/src/components/ErrorImageLocal/ErrorImageLocal.js
+++ frontend/src/components/ErrorImageLocal/ErrorImageLocal.js
@@ -3,7 +3,7 @@ import * as S from './ErrorImageLocal.styles';
 
 const ErrorImageLocal = () => (
   <S.ErrorImageLocalContainer>
-    <img src='/images/no-signal-image.png' alt="error" />
+    <img src='/images/no-signal-image.png' alt="error" data-test-id= "error-image"/>
     <S.ErrorImageLocalMessage>error</S.ErrorImageLocalMessage>
   </S.ErrorImageLocalContainer>
 );
